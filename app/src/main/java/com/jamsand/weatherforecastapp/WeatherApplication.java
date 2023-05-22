1f29e26ce44ccd12a4f0fe585e258a0fa101827b
odwa <odwa.menziwa@jumpco.io>
2023-05-22 16:51:06 +0200

    created a singleton application class

---
+++ app/src/main/java/com/jamsand/weatherforecastapp/WeatherApplication.java
@@ -0,0 +1,17 @@
+package com.jamsand.weatherforecastapp;
+
+import android.app.Application;
+
+public class WeatherApplication  extends Application {
+
+    private static WeatherApplication appContext;
+
+    private WeatherApplication(){}
+
+    public static synchronized WeatherApplication getInstance(){
+        if (appContext == null)
+            appContext = new WeatherApplication();
+            return appContext;
+    }
+
+}
