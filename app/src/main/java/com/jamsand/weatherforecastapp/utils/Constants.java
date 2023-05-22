106921b1d10e56521739c4555c0dfb3ded76151c
odwa <odwa.menziwa@jumpco.io>
2023-05-22 17:08:42 +0200

    created a constans class

---
+++ app/src/main/java/com/jamsand/weatherforecastapp/utils/Constants.java
@@ -0,0 +1,7 @@
+package com.jamsand.weatherforecastapp.utils;
+
+public class Constants {
+
+    public static String BASE_URL = "api.openweathermap.org/data/2.5/weather?lat=35&lon=139"; //example
+    public static String API_KEY = "53f9d8e4213222cf517d86dc406d67fc ";
+}
