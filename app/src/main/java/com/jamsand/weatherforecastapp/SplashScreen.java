523264e803b55ea0fadae1940247feacafb3d41f
odwa <odwa.menziwa@jumpco.io>
2023-05-23 16:50:27 +0200

    splashcreen working but need to implement correct styles

--- app/src/main/java/com/jamsand/weatherforecastapp/SplashScreen.java
+++ app/src/main/java/com/jamsand/weatherforecastapp/SplashScreen.java
@@ -2,16 +2,24 @@ package com.jamsand.weatherforecastapp;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
 import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
 import android.os.Handler;
+import android.widget.Toast;
 
 public class SplashScreen extends AppCompatActivity implements LocationListener {
-
     LocationManager locationManager;
     String locationText = "";
     String locationLatitude = "";
@@ -22,29 +30,186 @@ public class SplashScreen extends AppCompatActivity implements LocationListener 
     AlertDialog.Builder dailog;
 
 
+    //testing git
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+
+
+
+        if(checkInternetConnectivity()) {
+
+            //Alert Dialog
+            AlertDialog.Builder alertDialog2 = new AlertDialog.Builder(
+                    SplashScreen.this);
+
+            // Setting Dialog Title
+            alertDialog2.setTitle("Notification");
+
+            // Setting Dialog Message
+            String string1 = "Give it seconds for your coordinates to update";
+
+            alertDialog2.setMessage(string1);
+
+            // Setting Icon to Dialog
+            alertDialog2.setIcon(R.drawable.weather_app);
+
+            // Setting Positive "Yes" Btn
+            alertDialog2.setPositiveButton("Continue",
+                    new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int which) {
+
+                        }
+                    });
+
+            // Showing Alert Dialog
+            alertDialog2.show();
+
+            Handler handler2 = new Handler();
+            handler2.postDelayed(new Runnable() {
+                public void run() {
+                    mHandler = new Handler();
+                    startRepeatingTask();
+                }
+            }, 5000);   //5 seconds
+
+
+            //Permissions for getting Locations etc.
+
+            if (ContextCompat.checkSelfPermission(getApplicationContext(),
+                    android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED &&
+                    ActivityCompat.checkSelfPermission(getApplicationContext(),
+                            android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+
+                ActivityCompat.requestPermissions(this,
+                        new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION,
+                                android.Manifest.permission.ACCESS_COARSE_LOCATION}, 101);
+
+            }
+        } else {
+            showNoInternetDailog();
+        }
+    }
+    // getting location
+    public void getLocation() {
+        try {
+            locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,
+                    2000, 5, (LocationListener) this);
+        }
+        catch(SecurityException e) {
+            e.printStackTrace();
+        }
+    }
+    void startRepeatingTask() {
+        mStatusChecker.run();
+    }
+    void stopRepeatingTask() {
+        if(mHandler !=null)
+            mHandler.removeCallbacks(mStatusChecker);
     }
 
+    Runnable mStatusChecker = new Runnable() {
+        @Override
+        public void run() {
+
+
+            try {
+                getLocation(); //this function can change value of mInterval.
+
+                if (locationText.toString() == "") {
+                    Toast.makeText(getApplicationContext(), "Trying to retrieve coordinates...", Toast.LENGTH_SHORT).show();
+                }
+                else {
+
+                    Intent i = new Intent(SplashScreen.this, MainActivity.class);
+
+                    i.putExtra("latitude", locationLatitude);
+                    i.putExtra("longitude", locationLongitude);
+                    startActivity(i);
+
+                    // close this activity
+                    finish();
+
+                    System.out.println("lat == "+locationLatitude);
+                    System.out.println("lon == "+locationLongitude);
+                }
+            } finally {
+
+                mHandler.postDelayed(mStatusChecker, mInterval);
+            }
+        }
+    };
+
+
+
+
+    // Called when the location has changed.
     @Override
-    public void onLocationChanged(@NonNull Location location) {
+    public void onLocationChanged(Location location) {
+
+        locationText = location.getLatitude() + "," + location.getLongitude();
+        locationLatitude = location.getLatitude() + "";
+        locationLongitude = location.getLongitude() + "";
 
     }
 
     @Override
     public void onStatusChanged(String provider, int status, Bundle extras) {
-        LocationListener.super.onStatusChanged(provider, status, extras);
+
     }
 
     @Override
-    public void onProviderEnabled(@NonNull String provider) {
-        LocationListener.super.onProviderEnabled(provider);
+    public void onProviderEnabled(String provider) {
+        Toast.makeText(SplashScreen.this, "Please Enable GPS", Toast.LENGTH_SHORT).show();
     }
 
     @Override
-    public void onProviderDisabled(@NonNull String provider) {
-        LocationListener.super.onProviderDisabled(provider);
+    public void onProviderDisabled(String provider) {
+
     }
+
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        stopRepeatingTask();
+    }
+
+    // check internet connection for both MOBILE and WIFI
+    private boolean checkInternetConnectivity() {
+        boolean haveConnectedWifi = false;
+        boolean haveConnectedMobile = false;
+
+        ConnectivityManager cm = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo[] netInfo = cm.getAllNetworkInfo();
+        for (NetworkInfo ni : netInfo) {
+            if (ni.getTypeName().equalsIgnoreCase("WIFI"))
+                if (ni.isConnected())
+                    haveConnectedWifi = true;
+            if (ni.getTypeName().equalsIgnoreCase("MOBILE"))
+                if (ni.isConnected())
+                    haveConnectedMobile = true;
+        }
+        return haveConnectedWifi || haveConnectedMobile;
+    }
+    private void showNoInternetDailog(){
+        dailog = new AlertDialog.Builder(SplashScreen.this);
+        dailog.setMessage("No internet connection!");
+        dailog.setPositiveButton( "OK", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                finish();
+                dialog.dismiss();
+            }
+        });
+        AlertDialog alert = dailog.create();
+        alert.show();
+    }
+
+
+
+
 }
--- app/src/main/java/com/jamsand/weatherforecastapp/utils/Utilities.java
+++ app/src/main/java/com/jamsand/weatherforecastapp/utils/Utilities.java
@@ -1,17 +1,28 @@
 package com.jamsand.weatherforecastapp.utils;
 
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
+import android.content.pm.PackageManager;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 
+//import com.jamsand.weatherforecastapp.Manifest;
+//import com.jamsand.weatherforecastapp.SplashScreen;
 import com.jamsand.weatherforecastapp.WeatherApplication;
+
+//import androidx.core.app.ActivityCompat;
+//import androidx.core.content.ContextCompat;
 
 public class Utilities {
 
-  private Context context = WeatherApplication.getInstance();
+  private static Context context = WeatherApplication.getInstance();
 
-    // check internet connection for both MOBILE and WIFI
-    private boolean checkInternetConnectivity() {
+    AlertDialog.Builder dailog;
+
+
+    // check internet connection for both MOBILE and WIFI t
+    public static boolean checkInternetConnectivity() {
         boolean haveConnectedWifi = false;
         boolean haveConnectedMobile = false;
 
@@ -27,4 +38,34 @@ public class Utilities {
         }
         return haveConnectedWifi || haveConnectedMobile;
     }
+
+    public void showNoInternetDailog(){
+       // dailog = new AlertDialog.Builder(context.SplashScreen.this);
+        dailog.setMessage("No internet connection!");
+        dailog.setPositiveButton( "OK", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+       //         context.is;
+                dialog.dismiss();
+            }
+        });
+        AlertDialog alert = dailog.create();
+        alert.show();
+    }
+    public static void checkPermissions(){
+
+        //Permissions for getting Locations etc.
+
+//        if (ContextCompat.checkSelfPermission(context,
+//                Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED &&
+//                ActivityCompat.checkSelfPermission(context,
+//                        Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+//
+//            ActivityCompat.requestPermissions(context,
+//                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION,
+//                            Manifest.permission.ACCESS_COARSE_LOCATION}, 101);
+//
+    }
+
+
 }
